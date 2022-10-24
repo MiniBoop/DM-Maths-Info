44996e67ec91b96caaedf957578233795d641366
MiniBoop <sandra.augier.13@gmail.com>
2022-10-24 18:48:32 +0200

    activité1.3.py

--- activité1.3.py
+++ activité1.3.py
@@ -2,5 +2,16 @@
 
 N=int(input("Entrez un entier: "))
 
+def fact(N):
+    facto=1
+    for i in range(1,N+1):
+        facto= i*facto
+    return facto
+
 def TrianglePascal(N):
-    for i in range(N):
+    for i in range(N+1):
+        for j in range(N-i+1):
+            print()
+        for j in range(i+1):
+            print(fact(i)//(fact(j)*fact(i-j)),end=" ")
+print(TrianglePascal(N))
