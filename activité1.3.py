e32d16772087f0a0ac8cbba902c28bcdc1be3f89
MiniBoop <sandra.augier.13@gmail.com>
2022-10-24 19:27:02 +0200

    activité1.3.py

--- activité1.3.py
+++ activité1.3.py
@@ -10,8 +10,9 @@ def fact(N):
 
 def TrianglePascal(N):
     for i in range(N+1):
-        for j in range(N-i+1):
             print()
-        for j in range(i+1):
-            print(fact(i)//(fact(j)*fact(i-j)),end=" ")
+            for j in range(i+1):
+                print(fact(i)//(fact(j)*fact(i-j)),end=" ")
+    return ""
+
 print(TrianglePascal(N))
